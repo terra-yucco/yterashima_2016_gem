166798a480f57ae222ebcec38d3f50725b00a3d1
None <jojo1938043@gmail.com>
2016-07-30 08:26:19 +0000

    ADD CalcBMI Method

--- lib/yterashima_2016_gem.rb
+++ lib/yterashima_2016_gem.rb
@@ -1,5 +1,8 @@
 require "yterashima_2016_gem/version"
 
 module Yterashima2016Gem
-  # Your code goes here...
+  def self.calcBMI(height,weight)
+                puts "Your BMI is " + (weight/(height*height)).to_s
+  end
+
 end
